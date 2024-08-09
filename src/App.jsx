d024cd5aa513bcb1809557fa964c14f2de5f4899
mughni.s.kom <mughnimuhammad015@gmail.com>
2024-08-09 20:39:33 +0700

    latihan-4

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
+import { Container } from 'react-bootstrap';
 import './App.css'
-import { Routes, Route } from "react-router-dom"
-import Navbar from './components/NavBar'
-import Footer from './components/FooterComponent'
-import ButtonXs from './components/ButtonXs'
-import { useState, useEffect } from 'react'
+
+import { useState, useEffect, useRef } from 'react'
 
 
 function App() {
 
-  const [kado, setKado] = useState(0);
-  const [namaPrisiden, setNamaPrisiden] = useState("");
+  const lingRef = useRef(null)
 
-  useEffect(() => {
-    if (kado > 3) {
-      setNamaPrisiden('Mughni');
-    } else {
-      setNamaPrisiden('ikbal')
-    }
-  }, [kado])
+  const scrollTos = (ref) => {
+    scrollTo({
+      top: ref.offsetTop,
+      left: 0,
+      behavior: 'smooth'
+    })
+
+  }
 
   return (
     <>
-      <h1 className='text-center fw-bold text-warning pt-5'>Nama Presiden 2050 : {namaPrisiden}</h1>
-      <h1 className='fw-bold  text-center mt-5 pt-5 text-warning '>Nama Saya Mughni</h1>
-      <h1 className='text-center fw-bold text-warning'> Tahun : <i>{kado}</i> </h1>
+      <Container className='text-light text-center mt-5 border border-5 p-5 border-warning rounded-5'>
+        <img src="https://images.unsplash.com/photo-1633356122102-3fe601e05bd2?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width={300} className='mb-5 imgg border border-5 rounded-5' onClick={() => { scrollTos(lingRef.current) }} alt="" />
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla quibusdam reprehenderit magnam nemo consequuntur esse facere distinctio tempora dolore labore, debitis ipsa eaque quaerat perspiciatis repellat cumque eligendi. Mollitia exercitationem odit officia porro ipsa dolor, aliquid a quae officiis eaque animi necessitatibus quos odio! Voluptate ullam molestiae quisquam cum consequatur dicta provident nulla natus ea praesentium iste, tempore a, dolorem iure laborum officia beatae quae eum? Quae hic amet voluptatum voluptas? Ad, explicabo eum. Saepe earum debitis corporis optio ipsum, totam sapiente sed, quas magnam reiciendis eos accusantium neque animi quaerat nisi nihil quidem aliquam rerum voluptate laborum! Cum atque recusandae veniam optio nemo eos! Ipsum cum ipsam vero perferendis natus est quidem molestiae sequi facere dolore qui, nemo placeat, sed provident, unde quisquam dolorem nobis sit quibusdam maiores explicabo quos distinctio neque aperiam. Dolorum deserunt asperiores, iure saepe repellat, voluptas ut, sint aspernatur repellendus inventore sunt error numquam quidem recusandae assumenda? Soluta ut laboriosam impedit accusantium voluptates vitae sed cupiditate eligendi, minima cumque inventore, distinctio natus quibusdam nulla delectus iusto aspernatur maiores ad ducimus officia vel saepe, ullam eius excepturi. Voluptatem blanditiis nam at neque omnis minima expedita illum distinctio, tenetur ipsum, perferendis perspiciatis quisquam, quaerat deserunt ut iusto?</p>
 
-      <button onClick={() => setKado((kurang) => Math.max(kurang - 1, 0))}>Not enough</button>
-      <button onClick={() => setKado(0)}>Reset</button>
-      <button onClick={() => setKado(kado + 1)}>Plus</button>
+        <a href="#" target='_blank' className='pt-5' ref={lingRef}>React Js Broo</a>
+      </Container>
     </>
   )
 }
